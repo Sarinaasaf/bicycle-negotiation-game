3fe47a28426ae32cbc15e9fd9db9ce65a4646d0d
Sarinaasaf <deine-email@irgendwas.de>
2026-01-26 23:37:00 +0100

    fix client build (App.jsx)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// client/src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import IntroScreen from './pages/IntroScreen';
 import GroupSelection from './pages/GroupSelection';
 import WaitingRoom from './pages/WaitingRoom';
 import NegotiationScreen from './pages/NegotiationScreen';
 import ResultScreen from './pages/ResultScreen';
-import { GameProvider } from './context/GameContext';
 
-function App() {
+export default function App() {
   return (
-    <GameProvider>
-      <Router>
-        <div className="min-h-screen">
-          <Routes>
-            <Route path="/" element={<IntroScreen />} />
-            <Route path="/select-group" element={<GroupSelection />} />
-            <Route path="/waiting" element={<WaitingRoom />} />
-            <Route path="/negotiate" element={<NegotiationScreen />} />
-            <Route path="/result" element={<ResultScreen />} />
-          </Routes>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-        </div>
-      </Router>
-    </GameProvider>
+    <Router>
+      <ToastContainer position="top-right" autoClose={4000} />
+      <Routes>
+        <Route path="/" element={<IntroScreen />} />
+        <Route path="/select-group" element={<GroupSelection />} />
+        <Route path="/waiting" element={<WaitingRoom />} />
+        <Route path="/negotiate" element={<NegotiationScreen />} />
+        <Route path="/result" element={<ResultScreen />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
